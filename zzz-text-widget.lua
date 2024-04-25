eac01785185724f0a448142610e1b3c22a00e608
Evgeny <zobnin@gmail.com>
2024-04-25 09:13:13 +0400

    Add test widget

---
+++ zzz-text-widget.lua
@@ -0,0 +1,9 @@
+-- name = "Test widget"
+-- description = "Widget for testing purposes"
+-- type = "widget"
+-- author = "Tester 2000"
+-- version = "1.0"
+
+function on_resume()
+    ui:show_text("Test")
+end
