6c2c218fd1a3bd2cb37bfe08b3181b861d0475b4
Evgeny <zobnin@gmail.com>
2024-04-25 17:24:27 +0400

    Update test script

--- zzz-text-widget.lua
+++ zzz-text-widget.lua
@@ -2,7 +2,7 @@
 -- description = "Widget for testing purposes"
 -- type = "widget"
 -- author = "Tester 2000"
--- version = "1.1"
+-- version = "1.2"
 
 function on_resume()
     ui:show_text("Test")
