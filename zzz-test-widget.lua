34953c37e205029ffbd830d655ad88de4bbb1a96
Evgeny <zobnin@gmail.com>
2025-06-30 07:57:28 +0800

    Remove testing widget

--- zzz-test-widget.lua
+++
@@ -1,9 +0,0 @@
--- name = "Test widget"
--- description = "Widget for testing purposes"
--- type = "widget"
--- author = "Tester 2000"
--- version = "1.2"
-
-function on_resume()
-    ui:show_text("Test")
-end
